98b571aff24274026a0e1125318c94bc0b3066f0
danyi1212 <danyi1212@gmail.com>
2015-05-04 23:39:59 +0300

    all works!!!

--- js/Loader.js
+++ js/Loader.js
@@ -1,19 +1,17 @@
-document.write("<link rel='stylesheet' href='css/loaders.min.css'>");
-document.write("<link rel='stylesheet' href='css/loader.css'>");
-document.write("<div id='loadingScreen'><div id='loadAnination'><div class='loader-inner line-scale-pulse-out'><div></div><div></div><div></div><div></div><div></div></div></div></div>");
-(function () {
-    if (window.addEventListener) {
-        window.addEventListener("load", hideLoadingScreen, false);
-    }
-    else {
-        window.attachEvent("onload", hideLoadingScreen);
-    }
-})();
-
-
+//document.write("<link rel='stylesheet' href='css/loaders.min.css'>");
+//document.write("<link rel='stylesheet' href='css/loader.css'>");
+//document.write("<div id='loadingScreen'><div id='loadAnination'><div class='loader-inner line-scale-pulse-out'><div></div><div></div><div></div><div></div><div></div></div></div></div>");
 function hideLoadingScreen() {
     document.getElementById("loadingScreen").style.opacity = "0";
-    setTimeout(function(){
+    setTimeout(function () {
         document.getElementById("loadingScreen").style.display = "none";
     }, 6000);
 }
+
+(function () {
+    if (window.addEventListener) {
+        window.addEventListener("load", hideLoadingScreen, false);
+    } else {
+        window.attachEvent("onload", hideLoadingScreen);
+    }
+})();
