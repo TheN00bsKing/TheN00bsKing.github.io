5ecc4aa509cb899d610d054a93d2b3b5c2245358
danyi1212 <danyi1212@gmail.com>
2015-05-04 20:16:37 +0300

    Loading Screen

---
+++ js/Loader.js
@@ -0,0 +1,15 @@
+document.write("<link rel='stylesheet' href='css/loaders.min.css'>");
+document.write("<link rel='stylesheet' href='css/loader.css'>");
+document.write("<div id='loadingScreen'><div id='loadAnination'><div class='loader-inner line-scale-party'><div></div><div></div><div></div><div></div></div></div></div>");
+(function () {
+    if (window.addEventListener) {
+        window.addEventListener("load", hideLoadingScreen, false);
+    }
+    else {
+        window.attachEvent("onload", hideLoadingScreen);
+    }
+})();
+
+function hideLoadingScreen() {
+    document.getElementById("loadingScreen").style.opacity = "0";
+}
