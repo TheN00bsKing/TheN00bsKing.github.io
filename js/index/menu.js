0928aa3332c8f53cb54eec4c3e58fa1a19d6db87
danyi1212 <danyi1212@gmail.com>
2015-05-11 22:08:28 +0300

    redid menu fix in javascript

---
+++ js/index/menu.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var ul = document.getElementById("menuBT");
+    if (ul.style.display !== 'none') {
+        ul.style.display = 'none';
+    } else {
+        ul.style.display = 'block';
+    }
+}
