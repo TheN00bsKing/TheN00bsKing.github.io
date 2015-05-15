0d369e0128b610802c874c7c18ef83dc10425a68
danyi1212 <danyi1212@gmail.com>
2015-05-15 20:45:48 +0300

    gallery (only scripts)
    added gallery.
    all scripts are working.
    JSON file list all data.
    each image has a title, description and the image file.
    fully addable parameters such as date, rating, likes and more if needed.
    need to make the admin page to add images and redesign the gallery.

---
+++ js/gallery/gallery.js
@@ -0,0 +1,21 @@
+var images = JSON.parse(data);
+
+function printGallery() {
+    'use strict';
+    var grid = "<li class='grid-sizer'></li>", i;
+    for (i = 0; i < images.length; i++) {
+        grid += "<li> <figure> <img src='images/gallery/" + images[i].image + "' /> <figcaption> <h3>" + images[i].title + "</h3> <p>" + images[i].desciption + "</p> </figcaption> </figure> </li>";
+    }
+    document.getElementById("grid").innerHTML = grid;
+    
+    
+}
+
+function printSlideshow() {
+    'use strict';
+    var slide, i;
+    for (i = 0; i < images.length; i++) {
+        slide += "<li> <figure> <figcaption> <h3>" + images[i].title + "</h3> <p>" + images[i].desciption + "</p> </figcaption> <img src='images/gallery/" + images[i].image + "' /> </figure> </li>";
+    }
+    document.getElementById("slideshow").innerHTML = slide;
+}
