b88fa5976c7b4028b7aaf1d673747d41f3dfafd4
danyi1212 <danyi1212@gmail.com>
2015-05-16 19:24:11 +0300

    all working (yes I've used jquery...)

--- js/index/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var ul = document.getElementById("menuBT");
-    if (ul.style.height !== '0px') {
-        ul.style.height = '0px';
-    } else {
-        ul.style.height = 'auto';
-    }
-}
