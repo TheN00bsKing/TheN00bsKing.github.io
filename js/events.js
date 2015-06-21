643cf6dfb27f0e1c0f38e17757598b82e8020da2
danyi1212 <danyi1212@gmail.com>
2015-06-22 02:25:18 +0300

    add link
    more properties will be added in design. can access everything.

--- js/events.js
+++ js/events.js
@@ -14,15 +14,17 @@ window.fbAsyncInit = function() {
 }
 (document, 'script', 'facebook-jssdk'));
 
+var accessToken = "CAACEdEose0cBAFaCRrgl6UrAhv3SQqzexHoTwcOJumjpskbF3QZAoGkl1GI1DGbT63eV2I6ytKydCZBsvAeDfSHDJ7v6Gk7S3RRpmxRc7193ftU9ZC3yPU4nCt607TvJVpv7jrnlWDVryU9KpZB1N9qC3CBG5nq4IA4gkZBZC5XWhTdCwUSTTZBPIgNjRIBIXGOd1qcktSuXS20Bve5DZA8j2K7zZB64vXOgZD"
+
 function getEvents() {
     FB.api(
         "/Red.Sub.2003/events", 
-        {access_token: 'CAACEdEose0cBAFaCRrgl6UrAhv3SQqzexHoTwcOJumjpskbF3QZAoGkl1GI1DGbT63eV2I6ytKydCZBsvAeDfSHDJ7v6Gk7S3RRpmxRc7193ftU9ZC3yPU4nCt607TvJVpv7jrnlWDVryU9KpZB1N9qC3CBG5nq4IA4gkZBZC5XWhTdCwUSTTZBPIgNjRIBIXGOd1qcktSuXS20Bve5DZA8j2K7zZB64vXOgZD'},
+        {access_token: accessToken},
         function (response) {
             if (response && !response.error) {
                 for (var i = 0; i < response.data.length; i++) {
                     var event = response.data[i];
-                    $("#grid").append("<br>" + event.name);
+                    $("#grid").append("<br> <a href='https://www.facebook.com/events/" + event.id +"' >" + event.name + "</a>");
                 }
             } else {
                 alert(response.error.message);
