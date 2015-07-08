8a2b3cc3c1aebfd1d2a3b0da2efd9b8902c9e3da
danyi1212 <danyi1212@gmail.com>
2015-07-08 04:39:44 +0300

    new gallery data collector (Facebook graph API)

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -1,25 +1,74 @@
-var images = JSON.parse(data);
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '640939622674569',
+        xfbml      : true,
+        version    : 'v2.3'
+    });
+};
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}
+(document, 'script', 'facebook-jssdk'));
 
-function printGallery() {
+var accessToken = "640939622674569|0RxuhIyPrp9_DQyo-UQ9lI9R4YY";
+
+function getData(node, callback) {
     'use strict';
-    var grid = "<li class='grid-sizer'></li>", i;
-    for (i = 0; i < images.length; i++) {
-        grid += "<li> <figure> <img src='";
-        if(!images[i].image.indexOf("https://") == 0 && !images[i].image.indexOf("http://") == 0)
-            grid += "images/gallery/";
-        grid += images[i].image + "' /> <figcaption> <h3>" + images[i].title + "</h3> <p>" + images[i].description + "</p> </figcaption> </figure> </li>";
-    }
-    document.getElementById("grid").innerHTML = grid;
+    FB.api(
+        "/" + node, 
+        "GET",
+        {access_token: accessToken},
+        function (response) {
+            if (response && !response.error) {
+                callback(response);
+            } else {
+                alert(response.error.message);
+            }
+        }
+    );
 }
 
-function printSlideshow() {
-    'use strict';
-    var slide = "", i;
-    for (i = 0; i < images.length; i++) {
-        slide += "<li> <figure> <figcaption> <h3>" + images[i].title + "</h3> <p>" + images[i].description + "</p> </figcaption> <img src='";
-        if(!images[i].image.indexOf("https://") == 0 && !images[i].image.indexOf("http://") == 0)
-            slide += "images/gallery/";
-        slide += images[i].image + "' /> </figure> </li>";
-    }
-    document.getElementById("slideshow").innerHTML = slide;
+function getImage(id, callback) {
+    getData(id, function(response) {
+        alert(response.images[0].source);
+        callback(response.images[0].source);
+    });
+}
+
+function printAlbums() {
+    getData("Red.Sub.2003/albums", function(response) {
+        var albums = response.data;
+        alert(albums.length);
+        for (var i = 0; i < albums.length; i++) {
+            var album = albums[i];
+            encodeThumbnail(album);
+            alert(album.name);
+        }
+    });
+}
+
+function encodeThumbnail(album) {
+    var code = "<li><figure onclick='href('";
+    code += "" + album.link;
+    code += "');'><img src='";
+    var coverID = album.cover_photo;
+    getImage(coverID, function(response) {
+        alert(response);
+        code += "" + response; 
+    });
+    code += "'><figcaption><h3>";
+    code += "" + album.name;
+    code += "</h3><h4>";
+    code += album.count + "תמונות ";
+    code += "<h5><i class='fa fa-thumbs-up'></i>" + album.likes.data.length + "</h5>";
+    code += "</figcaption></figure></li>";
+    $("#grid").append(code);
+}
+
+function href(link) {
+    window.location.href = link;
 }
---
+++ js/oldGallery/gallery.js
@@ -0,0 +1,35 @@
+var images = JSON.parse(data);
+
+function printGallery() {
+    'use strict';
+    var grid = "<li class='grid-sizer'></li>", i;
+    for (i = 0; i < images.length; i++) {
+        grid += "<li> <figure> <img src='";
+        if(!images[i].image.indexOf("https://") == 0 && !images[i].image.indexOf("http://") == 0)
+            grid += "images/gallery/";
+        grid += images[i].image + "' /> <figcaption> <h3>" + images[i].title + "</h3> <p>" + images[i].description + "</p> </figcaption> </figure> </li>";
+    }
+    document.getElementById("grid").innerHTML = grid;
+}
+
+function printSlideshow() {
+    'use strict';
+    var slide = "", i;
+    for (i = 0; i < images.length; i++) {
+        slide += "<li> <figure> <figcaption> <h3>" + images[i].title + "</h3> <p>" + images[i].description + "</p> </figcaption> <img src='";
+        if(!images[i].image.indexOf("https://") == 0 && !images[i].image.indexOf("http://") == 0)
+            slide += "images/gallery/";
+        slide += images[i].image + "' /> </figure> </li>";
+    }
+    document.getElementById("slideshow").innerHTML = slide;
+}
+
+function getQueryVariable(variable) {
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
