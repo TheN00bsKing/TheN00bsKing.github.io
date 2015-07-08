7894d9dd577a1911c11ebaab3474fabc7c8c4b65
danyi1212 <danyi1212@gmail.com>
2015-07-09 02:27:53 +0300

    new gallery data collector (main code finished)
    need new grid system (working on it) and some design fixes (see issues)

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -1,23 +1,6 @@
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '640939622674569',
-        xfbml      : true,
-        version    : 'v2.3'
-    });
-};
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}
-(document, 'script', 'facebook-jssdk'));
-
 var accessToken = "640939622674569|0RxuhIyPrp9_DQyo-UQ9lI9R4YY";
 
 function getData(node, callback) {
-    'use strict';
     FB.api(
         "/" + node, 
         "GET",
@@ -32,41 +15,63 @@ function getData(node, callback) {
     );
 }
 
-function getImage(id, callback) {
-    getData(id, function(response) {
-        alert(response.images[0].source);
-        callback(response.images[0].source);
-    });
-}
-
-function printAlbums() {
-    getData("Red.Sub.2003/albums", function(response) {
-        var albums = response.data;
-        alert(albums.length);
-        for (var i = 0; i < albums.length; i++) {
-            var album = albums[i];
-            encodeThumbnail(album);
-            alert(album.name);
-        }
-    });
+function encodeLikeButton(link) {
+    var div = document.createElement("div");
+    div.setAttribute("class", "fb-like");
+    div.setAttribute("data-href", link);
+    div.setAttribute("data-width", "50");
+    div.setAttribute("data-layout", "standard");
+    div.setAttribute("data-action", "like");
+    div.setAttribute("data-show-faces", "true");
+    div.setAttribute("data-share", "true");
+    return div;
 }
 
 function encodeThumbnail(album) {
-    var code = "<li><figure onclick='href('";
-    code += "" + album.link;
-    code += "');'><img src='";
-    var coverID = album.cover_photo;
-    getImage(coverID, function(response) {
-        alert(response);
-        code += "" + response; 
+    
+    var h1 = document.createElement("h1");
+    var name = document.createTextNode(album.name);
+    h1.appendChild(name);
+    
+    var h2 = document.createElement("h2");
+    var count = document.createTextNode(album.count);
+    h2.appendChild(count);
+    
+    var likeButton = encodeLikeButton(album.link);
+    
+    var figcaption = document.createElement("figcaption");
+    figcaption.appendChild(h1);
+    figcaption.appendChild(h2);
+    figcaption.appendChild(likeButton);
+    
+    var image = album.photos.data[0].images[0].source;
+    var img = document.createElement('img');
+    img.setAttribute("src", "" + image);
+    
+    var figure = document.createElement("figure");
+    figure.setAttribute("onclick", "href(" + album.link + ");");
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    
+    var li = document.createElement("li");
+    li.setAttribute("id", album.id);
+    li.appendChild(figure);
+    
+    var grid = document.getElementById("grid");
+    grid.appendChild(li);
+    
+    FB.XFBML.parse(document.getElementById(album.id));
+}
+
+function printAlbums() {
+    getData("Red.Sub.2003/albums?fields=id, name, link, count, description, photos", function (response) {
+        var albums = response.data;
+        for (var i = 0; i < albums.length; i++) {
+            var album = albums[i];
+            encodeThumbnail(album);
+        }
     });
-    code += "'><figcaption><h3>";
-    code += "" + album.name;
-    code += "</h3><h4>";
-    code += album.count + "תמונות ";
-    code += "<h5><i class='fa fa-thumbs-up'></i>" + album.likes.data.length + "</h5>";
-    code += "</figcaption></figure></li>";
-    $("#grid").append(code);
+    
 }
 
 function href(link) {
