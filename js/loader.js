108c531f8e0ab85544475d2f1f04dfdfb76cdfc7
danyi1212 <danyi1212@gmail.com>
2015-07-09 21:03:08 +0300

    new encoding to loading screen
    to create the loading screen before the rest of the page the script must
    be the first line after the opening of the body tag.

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,33 @@
-document.write("<link rel='stylesheet' href='css/loader.css'><link rel='stylesheet' href='css/loaders.min.css'> <div id='loadingScreen'><div id='loadAnination'><div class='loader-inner line-scale-pulse-out'><div></div><div></div><div></div><div></div><div></div></div></div></div>");
+function encodeLoadingScreen() {
+    
+    var animation = document.createElement("div");
+    animation.setAttribute("class",  "loader-inner line-scale-pulse-out");
+    for (var i = 0; i < 5; i++) {
+        animation.appendChild(document.createElement("div"));
+    }
+    
+    var loadingAmination = document.createElement("div");
+    loadingAmination.setAttribute("id", "loadAnination");
+    loadingAmination.appendChild(animation);
+    
+    var loadingScreen = document.createElement("div");
+    loadingScreen.setAttribute("id", "loadingScreen");
+    loadingScreen.appendChild(loadingAmination);
+    
+    var loadCSS = document.createElement("link");
+    loadCSS.setAttribute("rel", "stylesheet");
+    loadCSS.setAttribute("href", "css/loader.css");
+    
+    var animationCSS = document.createElement("link");
+    animationCSS.setAttribute("rel", "stylesheet");
+    animationCSS.setAttribute("href", "css/loaders.min.css");
+    
+    var body = document.body;
+    body.appendChild(loadCSS);
+    body.appendChild(animationCSS);
+    body.appendChild(loadingScreen);
+    
+}
 
 function hideLoadingScreen() {
     document.getElementById("loadingScreen").style.opacity = "0";
@@ -8,6 +37,7 @@ function hideLoadingScreen() {
 }
 
 (function () {
+    encodeLoadingScreen();
     if (window.addEventListener) {
         window.addEventListener("load", hideLoadingScreen, false);
     } else {
